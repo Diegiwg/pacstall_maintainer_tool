a3584e437fb762916e20c24e59af0d005bc85b00
Diegiwg <diegiwg@gmail.com>
2023-06-18 21:06:15 -0300

    Initial commit for STD
    This commit includes the following changes:
    - Added the STD library, a collection of basic functions and constants for efficient script development.
    - Version: v0.1
    - Implemented the STD_test function for testing the STD library.
    
    Maintained by Diegiwg
    Copyright (c) 2023

---
+++ libs/std.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#
+# STD - Standard Library for Efficient Scripting
+#
+# A collection of basic functions and constants for efficient script development.
+#
+# Versions
+#
+# v0.1: Project Start, with the basic implementation of a test function to be imported, using `source $STD` and used in other scripts called by the pmt tool with `STD_test`.
+#
+# Maintained by Diegiwg
+# Copyright (c) 2023
+#
+
+function STD_test() {
+    echo "Testing STD library..."
+}
